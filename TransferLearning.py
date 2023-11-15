c6f6b179c55949ea5e78add116415163440df1a8
tanhhkhcndn <tanhhkhcndn@gmail.com>
2023-11-15 22:01:50 +0700

    Testing for custom dataset

--- TransferLearning.py
+++ TransferLearning.py
@@ -1,10 +1,30 @@
 from ultralytics import YOLO
+import ultralytics
+import torch
+print(torch.cuda.is_available())
+ultralytics.checks()
 
-model = YOLO('yolov8n.pt')
+# training
+# model = YOLO('yolov8n.pt')
+# if __name__ == '__main__':
+#     results = model.train(data='G:\\Code4Paper\\2023\\Yolo-Transfer-Learning\\Dataset\\hamster\\data.yaml', epochs=50, batch=8, device = 0)
+#     results = model.val()
+
+
+# testing
+ 
+# Load our custom goat model
+model = YOLO("runs/detect/train/weights/best.pt")
+ 
+# Use the model to detect object - goat
+model.predict(source="hamster.jpg", save=False, show=True)
+
+
 # 
 # results = model("testImage.jpg")  # return a list of Results objects
-results= model.predict('testImage.jpg', show=True, save=True, imgsz=320, conf=0.5)
-input("Press Enter to continue...")
+# results= model.predict('testImage.jpg', show=False, save=True, imgsz=320, conf=0.5)
+# print(len(model.names))
+# input("Press Enter to continue...")
 # Process results list
 # for result in results:
 #     boxes = result.boxes  # Boxes object for bbox outputs
